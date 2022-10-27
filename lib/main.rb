c78f02d6b99f4ca737828a88add98be43ac15b12
Olly Seber <olly_s@hotmail.co.uk>
2022-10-27 15:27:55 -0700

    Update driver script

--- lib/main.rb
+++ lib/main.rb
@@ -2,4 +2,8 @@
 
 require_relative "./knight_moves"
 
+puts "The shortest route between [0, 0] and [7, 7] is:"
 pp knight_moves([0, 0], [7, 7])
+
+puts "The shortest route between [2, 5] and [4, 7] is:"
+pp knight_moves([2, 5], [4, 7])
